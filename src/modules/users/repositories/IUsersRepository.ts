fe18d7b3ac9cea660e13f542bceab9aff23c64ce
Deibson Cogo <deibsoncogo@outlook.com>
2021-09-26 16:01:50 -0300

    Removendo os arquivos do desafio 3

--- src/modules/users/repositories/IUsersRepository.ts
+++
@@ -1,8 +0,0 @@
-import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from "../dtos";
-import { User } from "../entities/User";
-
-export interface IUsersRepository {
-  findUserWithGamesById(data: IFindUserWithGamesDTO): Promise<User | undefined>;
-  findAllUsersOrderedByFirstName(): Promise<User[]>;
-  findUserByFullName(data: IFindUserByFullNameDTO): Promise<User[] | undefined>;
-}
